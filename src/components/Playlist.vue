89297ab80dd774bbada6b7c5b1cc426c93c5daf3
wang48372162 <wang48372162@gmail.com>
2022-10-10 01:14:48 +0800

    Update playlist style

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="card">
-    <RouterLink :to="listTitleTo" class="block text-xl py-3 px-5 hover:bg-gray-50" data-test="list-title">
+    <RouterLink
+      :to="listTitleTo"
+      class="block py-3 px-5 hover:bg-gray-50 text-xl font-semibold transition duration-200"
+      data-test="list-title"
+    >
       {{ title }}
     </RouterLink>
 
@@ -8,8 +12,8 @@
       <li v-for="audio in audioList" :key="audio.id">
         <RouterLink
           :to="audioTo(audio.id)"
-          class="block py-3 px-5 hover:bg-gray-50"
-          :class="isActiveLink(audio.id) ? '' : ''"
+          class="block py-3 px-5 transition duration-200"
+          :class="isActiveLink(audio.id) ? 'bg-gray-600 text-white' : 'hover:bg-gray-50 text-gray-500 hover:text-gray-900'"
         >
           {{ audio.title }}
         </RouterLink>
